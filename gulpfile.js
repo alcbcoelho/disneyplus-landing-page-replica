49690d9f5f9a5df8612f27b3bbeffcdf32257946
André Coêlho <alcbcoelho@gmail.com>
2024-09-04 17:30:25 -0300

    Hero section setup + importing font files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
-function styles() {
+function generateStyles() {
     return gulp.src("./src/styles/*.scss")
         .pipe(sass({
             outputStyle: "compressed",
@@ -9,7 +10,13 @@ function styles() {
         .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
+function compressImages() {
+    return gulp.src("./src/images/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/images"));
+}
+
+exports.default = gulp.parallel(generateStyles, compressImages);
 exports.watch = function() {
-    gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+    gulp.watch("./src/styles/*.scss", gulp.parallel(generateStyles));
 }
