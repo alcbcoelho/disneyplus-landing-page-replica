417570464b88f9e757af8452c32d37d81abc7d5b
André Coêlho <alcbcoelho@gmail.com>
2024-09-06 01:48:25 -0300

    Production setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
 function generateStyles() {
     return gulp.src("./src/styles/*.scss")
@@ -16,7 +17,14 @@ function compressImages() {
         .pipe(gulp.dest("./dist/images"));
 }
 
-exports.default = gulp.parallel(generateStyles, compressImages);
+function compressScripts() {
+    return gulp.src("./src/scripts/**/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/js"));
+}
+
+exports.default = gulp.parallel(generateStyles, compressImages, compressScripts);
 exports.watch = function() {
     gulp.watch("./src/styles/*.scss", gulp.parallel(generateStyles));
+    gulp.watch("./src/scripts/*.scss", gulp.parallel(compressScripts));
 }
